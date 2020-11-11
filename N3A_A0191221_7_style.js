5c88830a0a136cba93d4930d52f8fd6a00ab8129
didi9535 <74306260+didi9535@users.noreply.github.com>
2020-11-12 01:24:55 +0900

    Add files via upload

---
+++ N3A_A0191221_7_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_N3A_A0191221_7 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "16.900000000000002px \'나눔스퀘어 ExtraBold\', sans-serif";
+    var labelFill = "#1ff30c";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("NAME") !== null) {
+        labelText = String(feature.get("NAME"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(114,155,111,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
